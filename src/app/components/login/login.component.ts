770ca799a68d5b45ecac216e8ae6c7d5e8ebe5bf
andrelugolopez <andrelugolopez@gmail.com>
2022-05-06 09:20:00 -0500

    creacion de spinner, localStore, autenticación

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,7 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { ClientService } from '../../services/client.service';
+import { AutorizacionService } from '../../services/autorizacion.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
+
+// nombres usados para seguridad
+// envio de token = into
+// nombre de usuario = Nuat
+// rol =n3yB6PZnGE8n7F
+// admin=J8p4SBfJgRfZCo
+// tecnico=H7qm7gQr6DBGfM
+// usuario=hbh2jFVsQM7RUy
 
 @Component({
   selector: 'app-login',
@@ -9,36 +19,67 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  mostrarSpinner: boolean = false;
+  mostrarFormulario: boolean = true;
+
   form: FormGroup = this.fb.group({/*se inicializa el form*/
   email: ['', Validators.required],
-  password: ['', Validators.required],
+  password: ['', Validators.required]
 
 });
-constructor(
+  
+constructor(/*inyeccion de independencias*/
+  private fb: FormBuilder,
   private client: ClientService,
-  private fb: FormBuilder /*inyeccion de independencias*/
+  public autorizacion: AutorizacionService,
+  private route: Router
   ){}
 
-  ngOnInit(): void {
-  }
-
-  onSubmit(){
-    if(this.form.valid){
-      let data={/**/
-        email:this.form.value.email,
-        password:this.form.value.password,
+      ngOnInit(): void {
+        this.form = this.fb.group({/*se inicializa el form*/
+          email: ['', Validators.required],
+          password: ['', Validators.required],
+        });
       }
 
-    this.client.sendFormToJson("http://127.0.0.1:5000/login",data
-    ).subscribe(
-    (response:any)=>console.log(response),
-    (error:any)=>console.log(error)
-  )
-      /*console.log("we",data)*/
-    }else{
+      async onSubmit(){
+        this.mostrarSpinner = true;
+        this.mostrarFormulario = false;
+        if(this.form.valid){
+      
 
-    }
-  }
+          // let data={/**/
+          //   email:this.form.value.email,
+          //   password:this.form.value.password,
+          // }
+
+          this.client.postRequest("http://127.0.0.1:5000/login",{
+            email:this.form.value.email,
+            password:this.form.value.password,
+          }).subscribe(
+
+            (response:any)=>{
+              console.log(response),
+              //token
+              this.autorizacion.login(response.into)
+              //nombres
+              this.autorizacion.setCourrentUser(response.Nuat)
+              //rol
+              this.autorizacion.setCourrentRol(response.n3yB6PZnGE8n7F)
+              this.route.navigate(['/']);
+
+          }),
+
+          (error:any)=> {
+            console.log(error);
+          };
+    
+          /*console.log("we",data)*/
+        }else{
+          console.log("Form error");
+
+        }
+     }
 
 }
 
--- src/app/services/client.service.spec.ts
+++ src/app/services/client.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { ClientService } from './client.service';
 
-describe('ClientService', () => {
+describe('ClienteService', () => {
   let service: ClientService;
 
   beforeEach(() => {

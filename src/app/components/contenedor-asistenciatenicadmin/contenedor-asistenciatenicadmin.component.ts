3a82ac19e513ce40be84fc4b810c38b137fa9a16
VikiCruz <bikiliseth2237@gmail.com>
2022-05-16 08:55:45 -0500

    se creo del nav el menu de perfil

---
+++ src/app/components/contenedor-asistenciatenicadmin/contenedor-asistenciatenicadmin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contenedor-asistenciatenicadmin',
+  templateUrl: './contenedor-asistenciatenicadmin.component.html',
+  styleUrls: ['./contenedor-asistenciatenicadmin.component.css']
+})
+export class ContenedorAsistenciatenicadminComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

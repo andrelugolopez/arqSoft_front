a3040e47029aabb4306a3dbabfa15f7c76917c7d
andrelugolopez <andrelugolopez@gmail.com>
2022-05-18 11:03:22 -0500

    modulo tecnico y orden de servicio

---
+++ src/app/components/contenedor-orden-servicio/contenedor-orden-servicio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contenedor-orden-servicio',
+  templateUrl: './contenedor-orden-servicio.component.html',
+  styleUrls: ['./contenedor-orden-servicio.component.css']
+})
+export class ContenedorOrdenServicioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

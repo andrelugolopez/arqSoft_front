39cca4833ae62991c4e5ea6cc6104f3d0f031452
Forero <forero5515@gmail.com>
2022-06-10 10:44:48 -0500

    asignación técnico parte1

--- src/app/components/asignacion-tecnico/asignacion-tecnico.component.ts
+++ src/app/components/asignacion-tecnico/asignacion-tecnico.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ClientService } from 'src/app/services/client.service';
 
 @Component({
   selector: 'app-asignacion-tecnico',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AsignacionTecnicoComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup = this.fb.group({/*se inicializa el form*/
+  codtecnico: ['', Validators.required],
+  nombretecnico: ['', Validators.required],
+  codservicio: ['', Validators.required],
+  escalarservicio: ['', Validators.required],
+  tipoespeciescalar: ['', Validators.required],
+  diaginicial: ['', Validators.required]
+});
 
-  ngOnInit(): void {
+constructor(
+  private client: ClientService,
+  private fb: FormBuilder /*inyeccion de independencias*/
+  ){}
+  ngOnInit(): void{
   }
+/**/
+  onSubmit(){
+    if(this.form.valid){
+      let data={/**/
+      codtecnico:this.form.value.codtecnico,
+      nombretecnico:this.form.value.nombretecnico,
+      codservicio:this.form.value.codservicio,
+      escalarservicio:this.form.value.escalarservicio,
+      tipoespeciescalar:this.form.value.tipoespeciescalar,
+      diaginicial:this.form.value.diaginicial
+     
+      }
 
+    this.client.postRequest("http://127.0.0.1:5000/asignacionTecnico",data
+    ).subscribe(
+    (response:any)=>console.log(response),
+    (error:any)=>console.log(error)
+  )
+      /*console.log("we",data)*/
+    }else{
+      console.log("Form error");
+    }
+  }
 }

44e1669ed0e1e7fa0d9ba0117021a7b10b410a8e
Forero <forero5515@gmail.com>
2022-06-16 11:57:57 -0500

    asignacion tecnico localstore

--- src/app/components/asignacion-tecnico/asignacion-tecnico.component.ts
+++ src/app/components/asignacion-tecnico/asignacion-tecnico.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ClientService } from 'src/app/services/client.service';
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
 
 
 @Component({
@@ -9,12 +11,17 @@ import { ClientService } from 'src/app/services/client.service';
   styleUrls: ['./asignacion-tecnico.component.css']
 })
 export class AsignacionTecnicoComponent implements OnInit {
-
+  data: any
+  date = new Date().toLocaleString().split(',')
+  fecha = this.date[0].replace(/\//g, "-")
+  hora = this.date[1]
+  
 
   form: FormGroup = this.fb.group({/*se inicializa el form*/
   codtecnico: ['', Validators.required],
   nombretecnico: ['', Validators.required],
   codservicio: ['', Validators.required],
+  tipodispositivo: ['', Validators.required],
   escalarservicio: ['', Validators.required],
   tipoespeciescalar: ['', Validators.required],
   diaginicial: ['', Validators.required]
@@ -25,8 +32,18 @@ export class AsignacionTecnicoComponent implements OnInit {
 constructor(
   private client: ClientService,
   private fb: FormBuilder /*inyeccion de independencias*/
-  ){}
+  ){
+    setInterval(() => {
+      this.hora = new Date().toLocaleString().split(',')[1]
+    }, 1)
+  }
+
+  
   ngOnInit(): void{
+    let infot = {
+        codte: localStorage.getItem("courrentDoc"),  
+      }
+      console.log("documento tecnico",infot)  
   }
 /**/
   onSubmit(){
@@ -35,11 +52,14 @@ constructor(
       codtecnico:this.form.value.codtecnico,
       nombretecnico:this.form.value.nombretecnico,
       codservicio:this.form.value.codservicio,
+      tipodispositivo:this.form.value.tipodispositivo,
       escalarservicio:this.form.value.escalarservicio,
       tipoespeciescalar:this.form.value.tipoespeciescalar,
       diaginicial:this.form.value.diaginicial
      
       }
+
+
 
     this.client.postRequest("http://127.0.0.1:5000/asignacionTecnico",data
     ).subscribe(

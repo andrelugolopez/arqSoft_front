713df99b522c7ca90f7bec3fea47ec459d4985b5
“andrelugo” <andrelugolopez@gmail.com>
2022-06-25 19:50:26 -0500

    actualizar productos

--- src/app/components/admin-articulos/admin-articulos.component.ts
+++ src/app/components/admin-articulos/admin-articulos.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientService } from '../../services/client.service';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ActivatedRoute, ParamMap} from '@angular/router';
+import { AutorizacionService } from '../../services/autorizacion.service';
+
+
 
 @Component({
   selector: 'app-admin-articulos',
@@ -7,9 +14,163 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminArticulosComponent implements OnInit {
 
-  constructor() { }
+  infoEquipos: any
+  producto: any
+
+  form: FormGroup = this.fb.group({/*se inicializa el form*/
+  idproducto: ['', Validators.required],
+  tipodispositivo: ['', Validators.required],
+  descripcionproductos: ['', Validators.required],
+  cantidad: ['', Validators.required],
+  nombre: ['', Validators.required],
+  precio: ['', Validators.required],
+  imagen: ['', Validators.required],
+
+  });
+
+  constructor(
+    private client: ClientService,
+    private fb: FormBuilder, /*inyeccion de independencias*/
+    private route: Router ,
+    public router: ActivatedRoute,
+    public autorizacion: AutorizacionService,
+  ) { }
 
   ngOnInit(): void {
+
+    
   }
+
+  Crear(){
+    if(this.form.valid){
+      let data={
+        idproducto:this.form.value.idproducto,
+        tipodispositivo:this.form.value.tipodispositivo,
+        descripcionproductos:this.form.value.descripcionproductos,
+        cantidad:this.form.value.cantidad,
+        nombre:this.form.value.nombre,
+        precio:this.form.value.precio,
+        imagen:this.form.value.imagen
+      }
+
+      this.client.postRequest('http://127.0.0.1:5000/crearproducto',data,this.autorizacion.getToken())
+      .subscribe(
+        (data:any) => {
+          console.log(data["data"]),
+          this.route.navigate(['/asistenciatenicadmin']);
+        },
+        (error:any)=>{
+          console.log(error)
+        });
+
+      }else{
+        console.log("Form error");
+      }
+    }
+
+    Actualizar(){
+      if(this.form.valid){
+        let data={
+          idproducto:this.form.value.idproducto,
+          tipodispositivo:this.form.value.tipodispositivo,
+          descripcionproductos:this.form.value.descripcionproductos,
+          cantidad:this.form.value.cantidad,
+          nombre:this.form.value.nombre,
+          precio:this.form.value.precio,
+          imagen:this.form.value.imagen
+        }
+  
+
+        console.log("token",this.autorizacion.getToken())
+  
+        this.client.postRequest('http://127.0.0.1:5000//actualizarProducto',data,this.autorizacion.getToken())
+        .subscribe(
+          (data:any) => {
+            console.log(data["data"]),
+            this.route.navigate(['/asistenciatenicadmin']);
+          },
+          (error:any)=>{
+            console.log(error)
+          });
+  
+        }else{
+          console.log("Form error");
+        }
+      }
+
+  Eliminar(){
+    if(this.form.valid){
+      let data={
+        idproducto:this.form.value.idproducto,
+        tipodispositivo:this.form.value.tipodispositivo,
+      }
+      console.log()
+
+      this.router.paramMap
+      .subscribe((params : ParamMap) => {
+
+      this.client.getRequest('http://127.0.0.1:5000/eliminarproducto?idproe='+this.form.value.tipodispositivo+this.form.value.idproducto,this.autorizacion.getToken())
+      .pipe()
+      .subscribe(
+        (data: any) => {
+          this.producto = this.fillForm(data["data"]),
+          this.route.navigate(['/asistenciatenicadmin']);
+          console.log(this.producto)
+          },
+          error => console.log("Ha ocurrido un error en la llamada: ", error)
+          )
+        });
+
+      }else{
+        console.log("Form error");
+      }
+    }
+
+
+
+  consultar(){
+    this.router.paramMap
+      .subscribe((params : ParamMap) => {
+
+      this.client.getRequest(`http://127.0.0.1:5000/productoId?idproducto=`+this.form.value.tipodispositivo+this.form.value.idproducto)
+      .pipe()  
+      .subscribe(
+        (data: any) => {
+          this.infoEquipos = this.fillForm(data["data"]),
+          console.log(data)
+          },
+          error => console.log("Ha ocurrido un error en la llamada: ", error)
+          )
+        });
+    }
+
+    public fillForm(values: any) {
+    // Diccionario
+    // Convierto las llaves de value a las llaves de form
+    const valueToForm: {[key: string]: string} = {
+
+
+      "descripcionproductos": "descripcionproductos",
+      "cantidad": "cantidad",
+      "nombre": "nombre",
+      "precio": "precio",
+      "rutaimagen": "imagen",
+
+    }
+    Object.entries(values).forEach(([name, value]) => {
+      // Obtengo la llave del formulario desde los values
+      // correo -> email
+      const formKey = valueToForm[name];
+
+      // Obtengo el control del formulario
+      const control = this.form.get(formKey) as FormControl;
+
+      // Si el control no existe o está modificado, retorno
+      if (!control || control.dirty) return;
+
+      // Defino el valor al control del formulario
+      control!.setValue(value);
+    });
+    }
 
 }
